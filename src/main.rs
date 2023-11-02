0d462798bdd02e0a155ad1c2759c681dc2f90302
KubikDezimeter <KubikDezimeter@gmail.com>
2023-11-02 22:52:04 +0100

    Add JavaScript

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use axum::{routing::get, Router, Server, response::{Html, IntoResponse}};
+use axum::{routing::get, Router, Server, response::{Html, IntoResponse}, http::Response};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
+    let router = Router::new()
+        .route("/", get(root_get))
+        .route("/index.js", get(indexjs_get));
     let server = Server::bind(&"0.0.0.0:8989".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
@@ -15,4 +17,14 @@ async fn root_get() -> impl IntoResponse {
     let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
 
     Html(markup)
+}
+
+#[axum::debug_handler]
+async fn indexjs_get() -> impl IntoResponse {
+    let javascript = tokio::fs::read_to_string("src/index.js").await.unwrap();
+    
+    Response::builder()
+        .header("content-type", "application/javascript;charset=utf-8")
+        .body(javascript)
+        .unwrap()
 }
