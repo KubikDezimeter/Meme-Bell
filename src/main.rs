915aa2bf552b6964c5219a8b2cb185be391aef1d
KubikDezimeter <KubikDezimeter@gmail.com>
2023-11-02 23:35:18 +0100

    Add index.css

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use axum::{routing::get, Router, Server, response::{Html, IntoResponse}, http::Response};
+use axum::{
+    http::Response,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router, Server,
+};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
-        .route("/index.js", get(indexjs_get));
+        .route("/index.js", get(indexjs_get))
+        .route("/index.css", get(indexcss_get));
     let server = Server::bind(&"0.0.0.0:8989".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
@@ -22,9 +28,19 @@ async fn root_get() -> impl IntoResponse {
 #[axum::debug_handler]
 async fn indexjs_get() -> impl IntoResponse {
     let javascript = tokio::fs::read_to_string("src/index.js").await.unwrap();
-    
+
     Response::builder()
         .header("content-type", "application/javascript;charset=utf-8")
         .body(javascript)
+        .unwrap()
+}
+
+#[axum::debug_handler]
+async fn indexcss_get() -> impl IntoResponse {
+    let css = tokio::fs::read_to_string("src/index.css").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "text/css;charset=utf-8")
+        .body(css)
         .unwrap()
 }
