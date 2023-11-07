fd405b9f9a95cb53f03be7b82e5784f0c5cf5d15
KubikDezimeter <KubikDezimeter@gmail.com>
2023-11-07 15:24:55 +0100

    Add method to play sounds on the local machine

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use axum::http::StatusCode;
+use axum::routing::post;
 use axum::{
     http::Response,
     response::{Html, IntoResponse},
     routing::get,
     Router, Server,
 };
+use rodio::{Decoder, OutputStream, Source};
+use std::fs::File;
+use std::io::BufReader;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
+const RINGING_TIME: u64 = 15;
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
         .route("/index.js", get(indexjs_get))
-        .route("/index.css", get(indexcss_get));
+        .route("/index.css", get(indexcss_get))
+        .route("/ring", post(ring_post));
     let server = Server::bind(&"0.0.0.0:8989".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
@@ -43,4 +54,23 @@ async fn indexcss_get() -> impl IntoResponse {
         .header("content-type", "text/css;charset=utf-8")
         .body(css)
         .unwrap()
+}
+
+async fn ring_post() -> impl IntoResponse {
+    println!("Bell is ringing");
+    let mp3 = File::open("sounds/megalovania.mp3").unwrap();
+    play_audio_file(mp3);
+
+    StatusCode::OK
+}
+
+/// Play the file 'sound' on the local machine
+fn play_audio_file(sound: File) {
+    thread::spawn(|| {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let bufreader = BufReader::new(sound);
+        let source = Decoder::new(bufreader).unwrap();
+        stream_handle.play_raw(source.convert_samples()).unwrap();
+        sleep(Duration::from_secs(RINGING_TIME));
+    });
 }
