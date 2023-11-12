0b1e8ff53acd31142dafac91e71ee5b163b2f385
KubikDezimeter <KubikDezimeter@gmail.com>
2023-11-12 03:18:22 +0100

    Show all available ringtones in a table

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,44 @@
 import { h, Component, render } from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
+import { useState, useEffect } from 'https://esm.sh/preact/hooks';
+
 
 // Initialize htm with Preact
 const html = htm.bind(h);
 
+// document.addEventListener("click", () => location.reload())
+
+function RingtoneList() {
+    const [ringtoneList, setRingtoneList] = useState([]);
+
+    useEffect(async () => {
+        await fetch("/api/get_ringtone_list")
+            .then((response) => response.json())
+            .then(setRingtoneList);
+    }, []);
+
+    return html`
+    <table>
+        <tr>
+            <th>Klingelton aktiv</th>
+            <th>Name</th>
+            <th></th>
+            <th></th>
+        </tr>
+        ${ringtoneList.map((ringtone) => html`
+        <tr>
+            <td><input type="checkbox" name="${ringtone}" /></td>
+            <td>${ringtone}</td>
+            <td><audio controls><source src="/api/get_ringtone/${ringtone}" type="audio/mpeg" />Your browser does not support the audio element.</audio></td>
+            <td><button type="button">Entfernen</button></td>
+        </tr>`)}
+    </table>`;
+}
+
 function App (props) {
     return html`
-        <h1>Hello ${props.name}!</h1>
-        <p>This is working with Preact now</p>
+        <h1>Meme Klingel</h1>
+        <${RingtoneList} />
     `;
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use axum::http::StatusCode;
+use axum::http::{header, HeaderValue, StatusCode};
 use axum::routing::post;
-use axum::{
-    http::Response,
-    response::{Html, IntoResponse},
-    routing::get,
-    Router, Server,
-};
+use axum::{http::Response, response::{Html, IntoResponse}, routing::get, Router, Server, Json, body};
 use rodio::{Decoder, OutputStream, Source};
 use std::fs::File;
-use std::io::BufReader;
-use std::thread;
+use std::io::{BufReader, Read};
+use std::{fs, thread};
+use std::path::PathBuf;
 use std::thread::sleep;
 use std::time::Duration;
+use axum::body::Full;
+use axum::extract::Path;
 
 const RINGING_TIME: u64 = 15;
 
@@ -21,7 +19,9 @@ async fn main() {
         .route("/", get(root_get))
         .route("/index.mjs", get(indexmjs_get))
         .route("/index.css", get(indexcss_get))
-        .route("/ring", post(ring_post));
+        .route("/ring", post(ring_post))
+        .route("/api/get_ringtone_list", get(api_get_ringtone_list))
+        .route("/api/get_ringtone/:ringtone", get(api_get_ringtone));
     let server = Server::bind(&"0.0.0.0:8989".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
     println!("Listening on {addr}");
@@ -58,10 +58,54 @@ async fn indexcss_get() -> impl IntoResponse {
 
 async fn ring_post() -> impl IntoResponse {
     println!("Bell is ringing");
-    let mp3 = File::open("sounds/megalovania.mp3").unwrap();
+    let mp3 = File::open("ringtones/megalovania.mp3").unwrap();
     play_ringtone(mp3);
 
     StatusCode::OK
+}
+
+#[axum::debug_handler]
+async fn api_get_ringtone_list() -> impl IntoResponse {
+    let paths = fs::read_dir("ringtones").unwrap();
+    let filenames: Vec<String> = paths.map(|path| path.unwrap().file_name().to_str().unwrap().to_string()).collect();
+    Json(filenames)
+
+
+    // Json(["megalovania.mp3", "Never Gonna Give You Up", "Nyan Cat", "500 Miles"])
+}
+
+async fn api_get_ringtone(Path(ringtone): Path<String>) -> impl IntoResponse {
+    let path = ringtone.trim_start_matches('/').to_string();
+    println!("Serving ringtones/{}", path);
+
+    let cargo_manifest_dir: String = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let ringtone_path = PathBuf::from(cargo_manifest_dir).join("ringtones").join(path.clone());
+    let ringtone_path: &std::path::Path = &ringtone_path.as_path();
+
+    return match ringtone_path.exists() {
+        true => {
+            let mut file_contents = Vec::new();
+            let mut file = File::open(&ringtone_path).expect("Unable to open audio file");
+            file.read_to_end(&mut file_contents).expect("Unable to read audio file");
+            Response::builder()
+                .status(StatusCode::OK)
+                .header(
+                    header::CONTENT_TYPE,
+                    HeaderValue::from_str("audio/mpeg").unwrap(),
+                )
+                .body(body::boxed(Full::from(file_contents)))
+                .unwrap()
+        },
+        false => {
+            println!("Requested resource ringtones/{} not found", path);
+            Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(body::boxed("Ringtone does not exist.".to_owned()))
+                .unwrap()
+        },
+    }
+
 }
 
 /// Play the file 'sound' on the local machine
