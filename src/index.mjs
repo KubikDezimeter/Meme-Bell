146f67597e7956f1201876e2241999c36a8a1476
jonathan.harmel <jonathan.harmel@nordakademie.de>
2023-11-25 07:42:09 +0100

    mjs: added Function toggleStatus, minor changes to asynchronous API Calls to avoid unexpected behavior, streamlined upload process into onClickOperation™ and added visibility functions for the components of the page HTML: removed "/" before script and css calls in order to use relative paths css: changed styles with blue as main color and orange as accent

--- src/index.mjs
+++ src/index.mjs
@@ -1,66 +1,111 @@
-import { h, Component, render } from 'https://esm.sh/preact';
+import {h, render} from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
-import { useState, useEffect } from 'https://esm.sh/preact/hooks';
-import { signal } from 'https://esm.sh/@preact/signals';
+import {useEffect, useState} from 'https://esm.sh/preact/hooks';
+import {signal} from 'https://esm.sh/@preact/signals';
 
 // Initialize htm with Preact
 const html = htm.bind(h);
 
-
 const ringtoneList = signal([]);
 
+async function fetchRingtoneList() {
+    const response = await fetch("/api/ringtone_list");
+    ringtoneList.value = await response.json();
+}
 function RingtoneList() {
-
-    useEffect(async () => {
-        ringtoneList.value = await fetch("/api/ringtone_list")
-            .then((response) => response.json())
+    useEffect(() => {
+        fetchRingtoneList();
     }, []);
 
-    return html`
-    <table>
-        <tr>
-<!--            <th>Klingelton aktiv</th>-->
-            <th>Name</th>
-            <th></th>
-            <th></th>
-        </tr>
-        ${ringtoneList.value.map((ringtone) => html`
-        <tr key="${ringtone}">
-<!--            <td><input type="checkbox" name="${ringtone}" /></td>-->
-            <td>${ringtone}</td>
-            <td><audio controls><source src="/api/ringtone/${ringtone}" type="audio/mpeg" />Your browser does not support the audio element.</audio></td>
-            <td><${RemoveButton} ringtone="${ringtone}" /></td>
-        </tr>`)}
-    </table>`;
-}
 
+    return html`
+        <div class="ringtone_table">
+        <main class="table">
+            <section class="table_header">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>name</th>
+                                  <th>sample</th>   
+                            <th>status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                </table>
+            </section>
+            <section class="table_body">
+                <table>
+                    <tbody>
+                        ${ringtoneList.value.map((ringtone, index) => html`
+                            <tr key="${ringtone}">
+                                <td class="index_container">${index + 1}</td>
+                                <td class="name_container">${ringtone}</td>
+                                <td class="audio_container"><audio controls><source src="/api/ringtone/${ringtone}" type="audio/mpeg" />Your browser does not support the audio element.</audio></td>
+                                <td class="status_container">
+                                    <button class="statusButton" onclick=${toggleStatus} style="border: none;">
+                                        <p class="status aktiv">aktiv</p>
+                                    </button>
+                                </td>
+                                <td class="removeButton_container"><${RemoveButton} ringtone="${ringtone}" /></td>
+                            </tr>
+                        `)}
+                    </tbody>
+                </table>
+            </section>
+        </main>
+    </div>`;
+}
+function toggleStatus(event) {
+    const statusContainer = event.target.closest('.status_container');
+    if (statusContainer) {
+        const statusElement = statusContainer.querySelector('.status');
+        if (statusElement) {
+            if (statusElement.classList.contains('aktiv')) {
+                statusElement.textContent = 'passiv';
+                statusElement.classList.remove('aktiv');
+                statusElement.classList.add('passiv');
+            } else {
+                statusElement.textContent = 'aktiv';
+                statusElement.classList.remove('passiv');
+                statusElement.classList.add('aktiv');
+            }
+        }
+    }
+}
 function RingtoneUploader() {
     const [file, setFile] = useState(null);
 
-    const handleFileChange = (e) => {
-        if (e.target.files) {
-            setFile(e.target.files[0]);
+    const handleFileChange = async (e) => {
+        console.log("handleFileChange started ");
+        if (e.target.files && e.target.files.length > 0) {
+            console.log("e");
+            const selectedFile = e.target.files[0];
+            setFile(selectedFile);
+
+            try {
+                console.log("handleUpload called");
+                await handleUpload(selectedFile);
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 
-    const handleUpload = async () => {
+    const handleUpload = async (file) => {
         if (file) {
             console.log("Uploading file...");
-
             const formData = new FormData();
             formData.append("file", file);
-
             try {
-                // You can write the URL of your server or any other endpoint used for file upload
                 const result = await fetch("/api/upload", {
                     method: "POST",
                     body: formData,
                 });
-
                 await result;
                 ringtoneList.value = [...ringtoneList.value, file.name];
-                setFile();
-                document.getElementById("file").value = "";
+                console.log(ringtoneList.value)
+                setFile(null);
             } catch (error) {
                 console.error(error);
             }
@@ -68,33 +113,22 @@ function RingtoneUploader() {
     };
 
     return html`
-        <div class="upload">
-        <div>
-            <label htmlFor="file" className="sr-only">
-                MP3 auswählen:
-            </label>
-            <input id="file" type="file" accept=".mp3" onChange=${handleFileChange} />
-        </div>
-        ${file && (html`
-        <section>
-            Ausgewählte Datei:
-            <ul>
-                <li>Name: ${file.name}</li>
-                <li>Größe: ${Math.round(file.size / (10**3))} kB</li>
-            </ul>
+        <section class="upload">
+            <div class="file-input">
+                <label for="file" class="chooseFileButton">
+                    <span>+</span>
+                    <input id="file" type="file" accept=".mp3" onChange=${(e) => handleFileChange(e)} />
+                </label>
+            </div>
         </section>
-    `)}
-
-    ${file && html`<button onClick=${handleUpload}>Upload</button>`}
-    </div>`;
+    `;
 }
-
 function RemoveButton(props) {
     const handleRemove = async () => {
         console.log("Moving ringtone " + props.ringtone + " to trash...");
 
         try {
-            const result = await fetch("/api/remove/" + props.ringtone, {
+            await fetch("/api/remove/" + props.ringtone, {
                 method: "POST",
             });
             ringtoneList.value = ringtoneList.value.filter((ringtone) => ringtone !== props.ringtone);
@@ -104,29 +138,21 @@ function RemoveButton(props) {
         }
     }
 
-    return html`<button type="button" onclick=${handleRemove}>Entfernen</button>`;
+    return html`<button class="removeButton" type="button" onclick=${handleRemove}>entfernen</button>`;
 }
-
-function Settings() {
-    return html`
-        <div class="settings">
-        <${RingingTimeSetting} />
-        </div>
-    `;
+async function fetchRingingTime() {
+    const response = await fetch("/api/settings/ringing_time");
+    return await response.json();
 }
-
 function RingingTimeSetting() {
     const [ringingTime, setRingingTime] = useState();
-    useEffect(async () => {
-        let value = await fetch("/api/settings/ringing_time")
-            .then((response) => response.json());
-        setRingingTime(value);
+    useEffect(() => {
+        fetchRingingTime().then(setRingingTime);
     }, []);
-
     const handleChange = async () => {
         let ringingTime = document.getElementById("ringingTime").value;
         try {
-            const result = await fetch("/api/settings/ringing_time", {
+            await fetch("/api/settings/ringing_time", {
                 method: "PUT",
                 body: ringingTime
             });
@@ -136,17 +162,31 @@ function RingingTimeSetting() {
     }
 
     return html`
-        <label for="ringingTime">Klingeldauer in Sekunden: </label>
-        <input type="number" id="ringingTime" min="1" value=${ringingTime} oninput=${handleChange} />
+        <input class="ringingTime_field" type="number" id="ringingTime" min="1" value="${ringingTime}" oninput="${handleChange}" placeholder="Klingeldauer in Sekunden" />
     `;
 }
+function toggleComponentsVisibility() {
+    const uploader = document.querySelector('.ringtoneUploader');
+    const list = document.querySelector('.ringtoneList');
 
-function App () {
+    uploader.classList.toggle('visible');
+    list.classList.toggle('visible');
+}
+
+function toggleSettingsVisibility() {
+    const settingsComponent = document.querySelector('.settingsComponent');
+    settingsComponent.classList.toggle('visible');
+}
+
+function App() {
     return html`
-        <h1>Meme Klingel</h1>
-        <${RingtoneList} />
-        <${RingtoneUploader} />
-        <${Settings} />
+        <h1 style="padding-bottom: 5px;" id="ringtoneTitle" onclick=${toggleComponentsVisibility}>RINGTONES</h1>
+        <div class="ringtoneUploader hidden">${RingtoneUploader()}</div>
+        <div class="ringtoneList hidden">${RingtoneList()}</div>
+        <h1 style="padding-top: 5px;" onclick=${toggleSettingsVisibility}>SETTINGS</h1>
+        <div class="settingsComponent hidden">
+            <${RingingTimeSetting} />
+        </div>
     `;
 }
 
